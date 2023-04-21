4b174b4921623521ad97f29ca02e3ee272638bfa
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-04-21 20:39:32 +0300

    profile

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from "react-dom";
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+ReactDOM.render(<App />, document.querySelector("#root")
 );
+
+
+
+
+
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
